31d02ab29d31573e2ef20e2f70d04a38b515a061
ecespede <eduard.cespedes@dsquare.be>
2014-10-13 09:56:39 +0200

    Initial Commit

---
+++ src/log4clojure/logging.clj
@@ -0,0 +1,212 @@
+(ns log4clojure.logging
+  (:require [couchdb-extension.couchdb :as couchdb]
+            [clojure.pprint :as pprint]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io])
+  (:import [ch.qos.logback.classic Level Logger]
+           [org.slf4j LoggerFactory MDC]
+           [java.io StringWriter]
+           [org.joda.time DateTime]))
+
+(def databaseName "hps-logging")
+
+(def errorDatabaseName "hps-error-logging")
+
+(def ^{:doc "Taken from the demo project
+  https://github.com/vaughnd/clojure-example-logback-integration/"}
+  logger ^ch.qos.logback.classic.Logger (LoggerFactory/getLogger "hps"))
+
+(defmacro debug [& msg]
+  `(.debug logger (print-str ~@msg)))
+
+(defmacro info [& msg]
+  `(.info logger (print-str ~@msg)))
+
+(defmacro error [throwable & msg]
+  `(if (instance? Throwable ~throwable)
+     (.error logger (print-str ~@msg) ~throwable)
+     (.error logger (print-str ~throwable ~@msg))))
+
+(defmacro spy
+  "Taken from the demo project
+  https://github.com/vaughnd/clojure-example-logback-integration/"
+  [expr]
+  `(let [a# ~expr
+         w# (StringWriter.)]
+     (pprint/pprint '~expr w#)
+     (.append w# " => ")
+     (pprint/pprint a# w#)
+     (error (.toString w#))
+     a#))
+
+(defn spit-content
+  "fileVector: is an array with the name of the file and the parameters.
+  f: is the function that you are wrapping around.
+  map: the values that are gonna be stored in the file.
+  (spit-content [\"get-tag-details-\" tagName] walk/keywordize-keys (historian/get-tag-details tagName))"
+  [fileVector f map]
+  (do
+    (spit (reduce #(str %1 "#" %2) fileVector) map)
+    (f map)))
+
+(defn slurp-content
+  [fileVector]
+  (->>
+    fileVector
+    (reduce #(str %1 "#" %2))
+    io/resource
+    slurp
+    json/read-str))
+
+(def log-agent (agent {:name "name"}))
+
+(def error-log-agent (agent nil))
+
+(defn- get-current-time-in-millis []
+  (->
+    (DateTime.)
+    (.getMillis)
+    str))
+
+(defn- persist-log-entry [entry]
+  (couchdb/store databaseName (get-current-time-in-millis) entry))
+
+(defn- persist-error-log-entry [entry]
+  (couchdb/store errorDatabaseName (get-current-time-in-millis) entry))
+
+(defn update-log-map [originalMap {dynamic :dynamic-token fix :fix-token remove :remove :as newValue}]
+  (let [bigKeyword (str fix ":" dynamic)
+        assocMap (assoc originalMap bigKeyword (merge (get originalMap bigKeyword) newValue))]
+    (if (nil? remove)
+      assocMap
+      (do
+        (->
+          assocMap
+          (get bigKeyword)
+          (dissoc :remove )
+          (persist-log-entry))
+        (dissoc originalMap bigKeyword)))))
+
+(defn update-map [old newMap] newMap)
+
+(def ^:dynamic *logPrinter* nil)
+
+(def serverup? (atom false))
+(def activeLogsMap (atom {}))
+
+(defn update-watch-agent [key reference old-state new-state]
+  (do
+    (when @serverup?
+      (swap! activeLogsMap update-log-map new-state))
+    (info (str new-state))))
+
+(defn write-error-logging [key reference old-state new-state]
+  (do
+    (when @serverup?
+      (persist-error-log-entry new-state))
+    (error (str new-state))))
+
+(defn init []
+  (do
+    (when (couchdb/server-is-up? databaseName)
+      (do
+        (couchdb/create-db databaseName)
+        (couchdb/create-db errorDatabaseName)
+        (swap! serverup? (fn [_ x] x) true)))
+    (add-watch log-agent :logging update-watch-agent)
+    (add-watch error-log-agent :logging write-error-logging)))
+
+(defn destroy []
+  (do
+    (remove-watch log-agent :logging )
+    (remove-watch error-log-agent :logging )))
+
+(defn trace-log [msg]
+  (send log4clojure.logging/log-agent log4clojure.logging/update-map
+    (merge log4clojure.logging/*logPrinter* msg)))
+
+(defmacro timelog [message [function & args :as all] & functions]
+  `(binding [log4clojure.logging/*logPrinter* {:fix-token (hash '~&form) :dynamic-token (rand-int 10000)}]
+     (let [start# (. System (nanoTime))
+           ret# ~all]
+       (send log4clojure.logging/log-agent log4clojure.logging/update-map
+         (merge {:ts (str (DateTime.))
+                 :message '~message
+                 :timeMillis (/ (double (- (. System (nanoTime)) start#)) 1000000.0)
+                 :namespace ~(str *ns*)
+                 :function '~function
+                 :remove "remove cache"}
+           log4clojure.logging/*logPrinter*))
+       ret#)))
+
+(defmacro timelog-complete [message [function & args :as all] & functions]
+  `(binding [log4clojure.logging/*logPrinter* {:fix-token (hash '~&form) :dynamic-token (rand-int 10000)}]
+     (let [start# (. System (nanoTime))
+           ret# ~all]
+       (send log4clojure.logging/log-agent log4clojure.logging/update-map
+         (merge {:ts (str (DateTime.))
+                 :message '~message
+                 :timeMillis (/ (double (- (. System (nanoTime)) start#)) 1000000.0)
+                 :namespace ~(str *ns*)
+                 :function '~function
+                 :args '~args
+                 :result ret#
+                 :form '~&form
+                 :env '~(str &env)
+                 :remove "remove cache"}
+           log4clojure.logging/*logPrinter*))
+       ret#)))
+
+(defmacro log-error [exception-type [function & args :as all] & functions]
+  `(try
+     ~all
+     (catch ~exception-type e#
+       (send log4clojure.logging/error-log-agent log4clojure.logging/update-map
+         {:ts (str (DateTime.))
+          :namespace ~(str *ns*)
+          :functionName (:name (meta #'~function))
+          :file (:file (meta #'~function))
+          :column (:column (meta #'~function))
+          :line (:line (meta #'~function))
+          :arglists (:arglists (meta #'~function))
+          :env '~(str &env)
+          :cause (.getCause e#)
+          :errorMessage (.getMessage e#)
+          :stackTrace (.toString e#)
+          :statTraceList (mapv #(str %) (.getStackTrace e#))}))))
+
+(defmacro log-error-return [return-message [function & args :as all] & functions]
+  `(try
+     ~all
+     (catch Exception e# (do
+                           (send log4clojure.logging/error-log-agent log4clojure.logging/update-map
+                             {:ts (str (DateTime.))
+                              :namespace ~(str *ns*)
+                              :functionName (:name (meta #'~function))
+                              :file (:file (meta #'~function))
+                              :column (:column (meta #'~function))
+                              :line (:line (meta #'~function))
+                              :arglists (:arglists (meta #'~function))
+                              :env '~(str &env)
+                              :cause (.getCause e#)
+                              :errorMessage (.getMessage e#)
+                              :stackTrace (.toString e#)
+                              :statTraceList (mapv #(str %) (.getStackTrace e#))})
+                           (when-not (nil? log4clojure.logging/*logPrinter*)
+                             (send log4clojure.logging/log-agent log4clojure.logging/update-map
+                               (merge {:ts (str (DateTime.))
+                                       :message "Exception!"
+                                       :exception true
+                                       :namespace ~(str *ns*)
+                                       :remove "remove cache"}
+                                 log4clojure.logging/*logPrinter*)))
+                           (if (nil? (ex-data e#))
+                             '~return-message
+                             (ex-data e#))))))
+
+(defmacro log-error-http [[function & args :as all] & functions]
+  `(log-error-return {:status 500
+                      :headers {"Content-Type" "application/json"}
+                      :body (json/write-str "There's been an Exception")}
+     ~all))
+
---
+++ test/log4clojure/logging_test.clj
@@ -0,0 +1,98 @@
+(ns log4clojure.logging-test
+  (:use clojure.test
+        midje.sweet)
+  (:require [log4clojure.logging :as log]
+            [couchdb-extension.couchdb :as couchdb]))
+
+(fact "How to merge multiple logs when we already have an entry"
+  (let [val-0 {"-259445176:7307" {:pointsFromHistorian 105
+                                  :keyTagsFromUi {:tags [{:name "isvag-tag0-hourly-1000-compressed"
+                                                          :range 96.1000013, :searchType 1}]
+                                                  :timePeriodFilters []
+                                                  :originalStartDate "2013-01-04T05:40:46.926Z"
+                                                  :originalEndDate "2013-01-07T04:24:28.155Z"}
+                                  :fix-token -259445176, :dynamic-token 7307}}
+        val-1 {:dynamic-token 7307
+               :fix-token -259445176
+               :ts "2014-08-08T10:44:34.880+02:00"
+               :namespace "hps.handler"
+               :function "->"
+               :message "POST /api/searchresult/"
+               :timeMillis 209.994}
+        response {"-259445176:7307" {:pointsFromHistorian 105
+                                     :keyTagsFromUi {:tags [{:name "isvag-tag0-hourly-1000-compressed"
+                                                             :range 96.1000013, :searchType 1}]
+                                                     :timePeriodFilters []
+                                                     :originalStartDate "2013-01-04T05:40:46.926Z"
+                                                     :originalEndDate "2013-01-07T04:24:28.155Z"}
+                                     :ts "2014-08-08T10:44:34.880+02:00"
+                                     :namespace "hps.handler"
+                                     :function "->"
+                                     :message "POST /api/searchresult/"
+                                     :timeMillis 209.994
+                                     :fix-token -259445176
+                                     :dynamic-token 7307}}]
+    (log/update-log-map val-0 val-1) => response))
+
+(fact "Check what happens the first time we create the keyword"
+  (let [val-1 {:dynamic-token 7307
+               :fix-token -259445176
+               :ts "2014-08-08T10:44:34.880+02:00"
+               :namespace "hps.handler"
+               :function "->"
+               :message "POST /api/searchresult/"
+               :timeMillis 209.994}
+        response {"-259445176:7307" {:dynamic-token 7307
+                                     :fix-token -259445176
+                                     :ts "2014-08-08T10:44:34.880+02:00"
+                                     :namespace "hps.handler"
+                                     :function "->"
+                                     :message "POST /api/searchresult/"
+                                     :timeMillis 209.994}
+                  "666:999" {:old "value"}}]
+    (log/update-log-map {"666:999" {:old "value"}} val-1) => response))
+
+(fact "When we finalize we should assoc the values first, persist the data and remove it from the map"
+  (let [val-0 {"-259445176:7307" {:dynamic-token 7307
+                                  :fix-token -259445176
+                                  :ts "2014-08-08T10:44:34.880+02:00"
+                                  :namespace "hps.handler"
+                                  :function "->"
+                                  :timeMillis 209.994}
+               "666:999" {:old "value"}}
+        val-1 {:dynamic-token 7307
+               :fix-token -259445176
+               :message "Do you know that Eduard is the best?"
+               :remove "remove"}
+        response {"666:999" {:old "value"}}]
+    (log/update-log-map val-0 val-1) => response
+    (provided (couchdb/store "hps-logging" anything
+                {:dynamic-token 7307
+                 :fix-token -259445176
+                 :ts "2014-08-08T10:44:34.880+02:00"
+                 :namespace "hps.handler"
+                 :function "->"
+                 :message "Do you know that Eduard is the best?"
+                 :timeMillis 209.994}) => anything)))
+
+
+;  Until we don't refactor the code the macro is not that small any more....
+;  Actual: "(try (str \"Edu\") (catch NullPointerException e__10557__auto__
+;                  (clojure.core/send log4clojure/error-log-agent log4clojure/update-map
+;                  {:statTraceList (clojure.core/mapv (fn* [p1__10556__10558__auto__]
+;                  (clojure.core/str p1__10556__10558__auto__)) (.getStackTrace e__10557__auto__)),
+;                  :cause (.getCause e__10557__auto__), :errorMessage (.getMessage e__10557__auto__), :file
+;                  (:file (clojure.core/meta (var str))), :env (quote \"\"),
+;                  :column (:column (clojure.core/meta (var str))),
+;                  :ts (clojure.core/str (org.joda.time.DateTime.)),
+;                  :line (:line (clojure.core/meta (var str))),
+;                  :arglists (:arglists (clojure.core/meta (var str))),
+;                  :namespace \"hps.test.util.log\", :functionName (:name (clojure.core/meta (var str))),
+;                  :stackTrace (.toString e__10557__auto__)})))"
+;  (fact "How to use log-error macro and what to expect from it"
+;    (->
+;      (log/log-error NullPointerException (str "Edu"))
+;      quote
+;      macroexpand
+;      str) =>
+;    #"try \(str \"Edu\"\) \(catch NullPointerException e__([0-9]+)__auto__ \(log/error e__([0-9]+)__auto__\)")
